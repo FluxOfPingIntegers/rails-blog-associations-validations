e94a8e21ef18764aedca2ba83ac702d32ff9529b
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-06-24 16:45:35 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,16 +25,21 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    if @post.save
+      redirect_to @post
+    else
+      render new_post_path
     end
+
+  #  respond_to do |format|
+  #    if @post.save
+  #      format.html { redirect_to @post, notice: 'Post was successfully created.' }
+  #      format.json { render action: 'show', status: :created, location: @post }
+  #    else
+  #      format.html { render action: 'new' }
+  #      format.json { render json: @post.errors, status: :unprocessable_entity }
+  #    end
+  #  end
   end
 
   # PATCH/PUT /posts/1
@@ -69,6 +74,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content, :tag_ids)
     end
 end
